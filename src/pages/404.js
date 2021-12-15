958b1fecedd1b7db3b063e1bed6ed5fb238c4988
teddywilson <hello@teddywilson.me>
2021-12-14 22:15:50 -0500

    404 comment

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Index from './index.js';
 
-// TODO(teddywilson) Implement a real 404 page. For now return home page component.
+/**
+ * Force renders Index component; 404 page is not needed at the moment.
+ */
 export default () => {
   return <Index />;
 };
