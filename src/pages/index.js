e4eb50323cc4ed2737af220a6df687d9a93676e9
teddywilson <hello@teddywilson.me>
2022-02-02 11:25:00 -0500

    passable lol idgaf

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,21 @@ const Index = () => {
         <link rel="canonical" href="http://teddywilson.me" />
       </Helmet>
       <div className="content">
-        <div className="header">Teddy Wilson</div>
-        <div className="subheader">NYC</div>
-        <div>+ engineered  ^ produced  ! mastered  * art</div>
+        <div className="header">teddy wilson</div>
+        <div className="subheader">
+          <div className="subheader-nyc">nyc</div>
+          <div className="subheader-divider">&nbsp;|&nbsp;</div>
+          <div className="subheader-email"><a href="mailto:hello@teddywilson.me">hello@teddywilson.me</a></div>
+        </div>
+        <div className="key">+ engineered&nbsp;&nbsp;^ produced&nbsp;&nbsp;! mastered&nbsp;&nbsp;* art</div>
         <div className="date">2020</div>
         <ul>
-          <li><a><i>reynolds</i> - Off-Peak Day Return LP [+^]</a></li>
-          <li><a><i>Linocut</i> – S/T EP [!*]</a></li>
+          <li><a href="https://reynolds-band.bandcamp.com/album/off-peak-day-return"><i>reynolds</i> - "Off-Peak Day Return" [+^]</a></li>
+          <li><a href="https://linocutlinocut.bandcamp.com/album/linocut"><i>Linocut</i> – "Linocut" [!*]</a></li>
         </ul>
         <div className="date">2018</div>
         <ul>
-          <li><a><i>reynolds</i> – Intent EP [+^!*]</a></li>
+          <li><a href="https://reynolds-band.bandcamp.com/album/intent"><i>reynolds</i> – "Intent" [+^!*]</a></li>
         </ul>
       </div>
     </div>
