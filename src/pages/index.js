814b24c0dbc1591f8c8bfdda0b3c9047d42883e5
Teddy Wilson <hello@teddywilson.me>
2022-05-03 08:49:36 -0400

    Remove content for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,41 +21,6 @@ const Index = () => {
         <img src={Me} height={500} style={{
           marginTop: 8
         }} />
-        <div className="key">{[
-          ['+', 'engineered'],
-          ['^', 'produced'],
-          ['!', 'mastered'],
-          ['*', 'art'],
-        ]
-          .reduce((prev, entry) => {
-            prev.push(
-              <><b>{entry[0]}</b>[<i>{entry[1]}</i>]&nbsp;</>,
-            )
-            return prev
-          }, [])}</div>
-        {/* TODO(teddywilson): Lol @ the following 'raw' html. Eventually move into some kind of CMS; works for now. */}
-        <div className="date">2022</div>
-        <ul>
-          <li><a href="https://drive.google.com/file/d/1Q469iHmKDrnSpRKGwnA8-lBaC-DQsO6n/view?usp=sharing">Favmily Vision / Blood tour flier [*]</a></li>
-        </ul>
-        <div className="date">2020</div>
-        <ul>
-          <li><a href="https://reynolds-band.bandcamp.com/album/off-peak-day-return"><i>reynolds</i> – "Off-Peak Day Return" [+^]</a></li>
-          <li><a href="https://linocutlinocut.bandcamp.com/album/linocut"><i>Linocut</i> – "Linocut" [!*]</a></li>
-        </ul>
-        <div className="date">2019</div>
-        <ul>
-          <li><a href="https://familyvision.bandcamp.com/album/geotag-image"><i>Family Vision</i> – "Geotag / Image" [!]</a></li>
-        </ul>
-        <div className="date">2018</div>
-        <ul>
-          <li><a href="https://reynolds-band.bandcamp.com/album/intent"><i>reynolds</i> – "Intent" [+^!*]</a></li>
-        </ul>
-        <div className="date">2017</div>
-        <ul>
-          <li><a href="https://soundcloud.com/familyvision/sets/split-family-vision-reynolds"><i>Family Vision / reynolds</i> – split [+^*]</a></li>
-        </ul>
-      </div>
     </div>
   );
 };
