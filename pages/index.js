b7f3c2897bee37ef7b14934c1664dff2c1408e3b
Teddy Wilson <hello@teddywilson.me>
2025-03-24 12:27:29 -0300

    test

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="description" content="Artist based in Brooklyn, New York" />
         <link rel="icon" type="image/svg" href="/favicon.svg" />
       </Head>
-      <main className="px-8 md:px-32 py-8 md:py-12 text-left">
+      <main className="px-8 md:px-32 py-9 md:py-12 text-left">
         <header className="mb-8 md:mb-16">
           <h1 className="text-2xl font-semibold mb-4">Teddy Wilson</h1>
           <p className="text-sm text-gray-900">Artist based in Brooklyn, New York</p>
